6433663b6ad81af13d11bf8806d6b74fc735c8ee
Antonio Lisboa de Carvalho <antoniol.carvalho49@gmail.com>
2023-04-07 18:36:00 -0300

    Aula 02
    Introdução a objetos

---
+++ Aula02/src/aula02/Aula02.java
@@ -0,0 +1,20 @@
+
+package aula02;
+
+
+public class Aula02 {
+
+
+    public static void main(String[] args) {
+        
+        Caneta c1 = new Caneta();
+        
+        c1.cor = "Azul";
+        c1.ponta = (float)0.5;
+        c1.destampar();
+        c1.rabiscar();
+        c1.status();
+
+    }
+    
+}
